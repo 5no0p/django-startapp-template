50f8d6e6abe3d4809d0b6f1b78913bd300a25e06
5no0p <mmoaaa1989@hotmail.com>
2021-12-14 20:54:34 +0200

    modify apps.py and api/v1/urls.py
    solve space case in apps.py and app_name to urls.py commented example

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from {{ app_name}}.api.v1 import views
 
 router = routers.DefaultRouter()
-#router.register(r'tasks', views.TaskViewSet)
+#router.register(r'{{ app_name}}', views.{{ camel_case_app_name }}ViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
